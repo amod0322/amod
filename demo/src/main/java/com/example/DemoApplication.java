f180525c4aa83bef92237a7d780a5d408de7a886
amodmurkute <amodmurkute@gmail.com>
2017-07-15 20:48:14 +0530

    done

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-
 @Disable
-
 public class DemoApplication {
 
 	public static void main(String[] args) {
